08f3f62f3c3c26954e6a6a7c5b4fa89f5fb88314
Brennon Redmyer <brennon@dsmedialabs.com>
2013-01-25 21:12:23 -0500

    cocos2d w/ box2d converted to arc

---
+++ ggj13/cocos2d-library/cocos2d_library.h
@@ -0,0 +1,13 @@
+//
+//  cocos2d_library.h
+//  cocos2d-library
+//
+//  Created by Brennon Redmyer on 1/25/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface cocos2d_library : NSObject
+
+@end
---
+++ ggj13/ggj13/AppDelegate.h
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.h
+//  ggj13
+//
+//  Created by Brennon Redmyer on 1/25/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "cocos2d.h"
+
+@interface AppController : NSObject <UIApplicationDelegate, CCDirectorDelegate>
+{
+	UIWindow *window_;
+	UINavigationController *navController_;
+	
+	CCDirectorIOS	*director_;							// weak ref
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (readonly) UINavigationController *navController;
+@property (readonly) CCDirectorIOS *director;
+
+@end
